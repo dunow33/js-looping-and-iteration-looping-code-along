f6bcff77c3b1f65e792b7d1bf3deaa00f48feac6
dunow <dunow33@hotmail.com>
2021-02-07 00:37:28 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    const stringArray = [];
+    for(let i = 0; i < names.length; i++)
+        stringArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+
+    return stringArray;
+}
+
+function countDown(integer) {
+    while(integer >= 0){
+        console.log(integer);
+        integer--;
+    }
+}
